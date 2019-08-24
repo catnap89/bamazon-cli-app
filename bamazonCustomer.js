5e5f88f6e368ce7ba5f948a338c75db84beabed9
youngwoo.cho <petshop0907@gmail.com>
2019-08-24 00:52:55 -0400

    using .env and mysqlPass.js, exported mysql password. Prevented mysql password to be pushed in github by adding .env to .gitignore.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,11 @@
 // ________________________________________
 // DEPENDENCIES
 // ========================================
+require('dotenv').config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+var mysqlPass = require("./mysqlPass");
+// var mysqlPass = new connection(mysqlPass.mysqlConnect); 
 // ________________________________________
 // mySQL
 // ========================================
@@ -11,7 +13,12 @@ var connection = mysql.createConnection({   // create the connection information
   host: "localhost",
   port: 3306,                               // Your port; if not 3306
   user: "root",                             // Your username
-  password: "",                             // Your password
+  password: mysqlPass.mysqlConnect,         // Your password stored in .env
   database: "bamazon"
 });
 
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
