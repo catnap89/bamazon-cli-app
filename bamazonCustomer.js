e4098a9f7fb2a13071816a6653b3d8c24ce76fad
youngwoo.cho <petshop0907@gmail.com>
2019-08-24 04:31:12 -0400

    After showProduct function is called, call purchase function to select the products in bamazon database's products table, after selecting product, ask the quantity user wants to puchase. If bamazon has enough stock quantity, update it by subtracting user quantity from the product's stock_quantity in the database as the product was sold to the user. And log the amount user spent. If stock_quantity is lower than user quantity, log that bamazon has insufficient stock quantity.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -38,7 +38,71 @@ function showProducts() {                   // Display All Products
       // console.log("ID: " + id + " Name: " + name + " Department: " + department + " Price: " + "$"+ price + " Quantity: " + quantity);
     }
     console.log(query.sql);       // logs the actual query being run
-    connection.end();
+    purchase();
   });
+}
+
+function purchase() {
+  connection.query("SELECT * FROM products", function(err, products) {    // query the database for all products being auctioned
+    if (err) throw err;
+
+    inquirer        // once you have the products, prompt the user for which product they would like to purchase and how many
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < products.length; i++) {
+              choiceArray.push(products[i].id);
+            }
+            return choiceArray;
+          },
+          message: "What product would you like to purchase?"
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to purchse?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < products.length; i++) {
+          if (products[i].id === answer.choice) {
+            chosenItem = products[i];
+          }
+        }
+
+        // determine if stock_quantity was enough for user to purchase quantity
+        if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+          // if stock_quantity was high enough, so update db, let the user know, and show the total cost to user, start over
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: chosenItem.stock_quantity - answer.quantity
+              },
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Successfully purchased product! You spent $" + chosenItem.price * answer.quantity);
+              // showProducts();
+              connection.end();
+            }
+          );
+        }
+        else {
+          // Stock_quantity wasn't high enough, so apologize and start over
+          console.log("Bamazon does have enough products in stock to complete your order. Try again...");
+          purchase();
+        }
+      });
+  });
+
 }
 
