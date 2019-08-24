b0741bf8cc4c59c17d0a42aaf222ca0a27a30bab
youngwoo.cho <petshop0907@gmail.com>
2019-08-24 00:05:01 -0400

    added connection variable that creates the connection information for the sql database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,3 +3,15 @@
 // ========================================
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+// ________________________________________
+// mySQL
+// ========================================
+var connection = mysql.createConnection({   // create the connection information for the sql database
+  host: "localhost",
+  port: 3306,                               // Your port; if not 3306
+  user: "root",                             // Your username
+  password: "",                             // Your password
+  database: "bamazon"
+});
+
