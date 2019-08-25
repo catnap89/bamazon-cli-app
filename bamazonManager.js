c0f969f939ec17f29ff81b3e25988a5fe37112ad
youngwoo.cho <petshop0907@gmail.com>
2019-08-25 04:15:59 -0400

    added removePrompt and removeProduct functions to remove product from the products table in bamazon database

--- bamazonManager.js
+++ bamazonManager.js
@@ -39,6 +39,7 @@ function menuOptions() {
           "View Low Inventory", 
           "Add to Inventory", 
           "Add New Product",
+          "Remove Product from Inventory",
           "Exit"
           ]
       }
@@ -68,6 +69,11 @@ function doOption(option) {
     case "Add New Product":
       newProductPrompt();
       break;
+    
+    case "Remove Product from Inventory":
+      removePrompt();
+      break;
+
     case "Exit":
       connection.end();
       break;
@@ -101,20 +107,26 @@ function displayLowInventory() {
   let tableData = [];
   connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
     if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      var id = "ID: " + res[i].id;
-      var name = "Name: " + res[i].product_name;
-      var department = "Department: " + res[i].department_name;
-      var price = "Price: $" + res[i].price;
-      var quantity = "Quantity: " + res[i].stock_quantity;
- 
-      let data;
-      data = [id, name, department, price, quantity];
-      tableData.push(data);
+    if (res.length > 0) {         // if there is at least one product that has lower than 5 stock quantity,
+      for (var i = 0; i < res.length; i++) {
+        var id = "ID: " + res[i].id;
+        var name = "Name: " + res[i].product_name;
+        var department = "Department: " + res[i].department_name;
+        var price = "Price: $" + res[i].price;
+        var quantity = "Quantity: " + res[i].stock_quantity;
+  
+        let data;
+        data = [id, name, department, price, quantity];
+        tableData.push(data);
+      }
+      var output = table(tableData);
+      console.log(output);
+      menuOptions();
+
+    } else {
+      console.log("All Items are available to sell!");
+      menuOptions();
     }
-    var output = table(tableData);
-    console.log(output);
-    menuOptions();
   })
 }
 
@@ -211,4 +223,60 @@ function addNewProduct(name, department, price, quantity) {
   console.log("Following product was added as a new product!\n" +
   "Name: " + name + " |" + " Department: " + department + " | " + " Price: " + price + " | " + " Quantity: " + quantity);
   menuOptions();
+}
+
+function removePrompt() {
+  inquirer
+    .prompt([
+      {
+        name: "ID",
+        type: "number",
+        message: "Input ID of the product you wish to remove from the inventory. The item will be removed completely!"
+      },
+      {
+        name: "confirmation",
+        type: "confirm",
+        message: "Are you sure you want to remove this item?",
+        default: false
+      }
+    ])
+    .then(function(res) {
+      var id = res.ID;
+      var confirmation = res.confirmation;
+      if (confirmation) {
+        removeProduct(id);
+      } else {
+        console.log("You cancelled removing product from the inventory");
+        menuOptions();
+      }
+    })
+}
+
+function removeProduct(ID) {
+
+  connection.query("SELECT * FROM products WHERE id=" + ID, function(err, res) {
+    if (err) throw err;
+
+    var id = "ID: " + res[0].id;
+    var name = "Name: " + res[0].product_name;
+    var department = "Department: " + res[0].department_name;
+    var price = "Price: $" + res[0].price;
+    var quantity = "Quantity: " + res[0].stock_quantity;
+    let data;
+
+    data = [
+      [id, name, department, price, quantity]
+    ];
+    var output = table(data);
+    console.log("\n\n" + "Following product has been removed from the inventory \n" + output + "\n");
+    connection.query(
+      "DELETE FROM products WHERE ?",
+      {
+        id: ID
+      }
+    )
+
+    menuOptions();
+  })
+
 }
